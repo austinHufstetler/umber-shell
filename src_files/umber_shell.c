15a7402129db25c720fba2930ee20a37bc7bca34
Austin Hufstetler <austinHufstetler@gmail.com>
2018-02-03 23:43:50 -0500

    organized folders, makefile, and added welcome screen

---
+++ src_files/umber_shell.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "umber_shell.h"
+
+void welcome(){
+        printf("\n\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        printf("\n");
+        printf("\t               Umber Shell                    \n");
+        printf("\n");
+        printf("\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+        printf("\n\n");
+}
+
+int main(){
+	welcome();
+	return 0;
+}
